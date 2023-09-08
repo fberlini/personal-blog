2aa60b3f99244748249ef5a9805def0d28091b68
Felipe Berlini <felipe.berlini@hotmail.com>
2023-09-08 13:41:49 +0200

    structuring main page

---
+++ blog-ui/src/Components/Post.tsx
@@ -0,0 +1,12 @@
+const Post = () => {
+  return (
+    <div className="post">
+      <h1>Este é um post de testes para o blog</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla euismod purus eget tellus porttitor vehicula. Maecenas eget dui dapibus, mattis nulla at, rhoncus orci. Maecenas nulla quam, accumsan sit amet consectetur ac, blandit vitae justo. Proin viverra tincidunt enim vel fringilla. Sed in hendrerit enim. Aenean malesuada turpis nec quam rutrum elementum. Curabitur interdum nisl vitae nisl finibus, eu ultricies ligula fermentum.</p>
+      <p>Ut malesuada metus eu justo tincidunt, at mattis nisi semper. Nam nec ultricies nulla, vitae mattis justo. Quisque vulputate volutpat diam non scelerisque. Donec sodales egestas ultricies. Donec auctor sed eros in laoreet. Nam risus tortor, fringilla nec ligula non, porta eleifend dui. Proin dictum, ante eu lobortis congue, dolor lacus elementum sem, dignissim ullamcorper ex justo vitae risus. Duis ante massa, euismod eu ex vitae, egestas fringilla lacus. Vivamus maximus tincidunt eros, et congue leo mattis eu. Nullam rhoncus dolor in massa fermentum porta. Fusce imperdiet lorem non est dignissim vehicula. Phasellus fringilla felis quis eros tincidunt, non aliquet felis sollicitudin. Praesent nec euismod lorem.</p>
+      <p>Ut tristique purus non sollicitudin rhoncus. Curabitur pellentesque lacus in nisi fringilla, quis gravida nulla condimentum. Cras quis vehicula tellus. Quisque feugiat diam eget rhoncus tincidunt. Sed orci ex, lacinia a mollis in, faucibus nec nisi. Proin tellus lorem, rhoncus a tempus ut, vulputate ultricies arcu. Suspendisse vestibulum orci rutrum ullamcorper scelerisque. Sed vel neque mauris. Curabitur mauris sem, eleifend at fermentum at, ullamcorper a mi. Proin mollis purus ac nisl elementum, id mattis dui bibendum.</p>
+    </div>
+  );
+};
+
+export default Post;
